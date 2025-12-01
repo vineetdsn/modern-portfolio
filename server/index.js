fb060305a9c7365d5a0f69186f11df20ed7cd03d
vineetdsn <vineetdsn@gmail.com>
2025-12-01 22:35:17 +0530

    the express container

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import { registerRoutes } from "./routes.js";
 import { setupVite, serveStatic, log } from "./vite.js";
 
 const app = express();
-
+// This is express
 app.use(express.json({
   verify: (req, _res, buf) => {
     req.rawBody = buf;
