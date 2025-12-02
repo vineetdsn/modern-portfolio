22bbdbb5160ad9858bd34ede1c1821c0b958e853
vineetdsn <vineetdsn@gmail.com>
2025-12-02 23:40:28 +0530

    page founded

--- client/src/pages/not-found.jsx
+++ client/src/pages/not-found.jsx
@@ -1,6 +1,6 @@
 import { Card, CardContent } from "@/components/ui/card";
 import { AlertCircle } from "lucide-react";
-
+// this is the not found content
 export default function NotFound() {
   return (
     <div className="min-h-screen w-full flex items-center justify-center bg-gray-50">
