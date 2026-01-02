cd92d6fc803727f546e260d53b6ff02ecef0fca5
vineetdsn <vineetdsn@gmail.com>
2026-01-02 18:03:41 +0530

    storage capacity

--- server/storage.js
+++ server/storage.js
@@ -4,7 +4,7 @@ export class MemStorage {
   constructor() {
     this.contactMessages = new Map();
   }
-
+// storage category
   async createContactMessage(message) {
     const id = randomUUID();
     const messageWithId = { ...message, id };
